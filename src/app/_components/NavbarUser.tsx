a9f5768e92b6cdc06fb9e9051fb688c8fb3b26d5
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-06-09 12:00:06 +0700

    fix: font

--- src/app/_components/NavbarUser.tsx
+++ src/app/_components/NavbarUser.tsx
@@ -1,4 +1,4 @@
-import { Home, ShoppingCart, User } from "lucide-react";
+import { Home, Lock, ShoppingCart, User } from "lucide-react";
 import Image from "next/image";
 import { Input } from "~/components/ui/input";
 
@@ -17,7 +17,7 @@ export const NavbarUser = () => {
           placeholder="ค้นหาพัสดุ"
         />
       </div>
-      <div className="grid w-full grid-cols-3 font-noto-sans">
+      <div className="grid w-full grid-cols-3 justify-items-center font-noto-sans">
         <a href="/users/profile">
           <User className="h-8 w-8 hover:cursor-pointer" />
         </a>
@@ -29,16 +29,9 @@ export const NavbarUser = () => {
         </a>
       </div>
       <div className="flex items-center justify-end gap-4">
-        <div className="flex h-11 w-11 items-center justify-start rounded-3xl bg-yellow-500 p-2">
-          <div className="relative h-7 w-7">
-            <div className="absolute left-0 top-0 h-7 w-7 bg-zinc-300"></div>
-          </div>
-        </div>
-        <div className="flex items-start justify-start gap-2.5 rounded-3xl bg-yellow-500 p-2.5">
-          <div className="relative h-6 w-6">
-            <div className="absolute left-0 top-0 h-6 w-6 bg-zinc-300"></div>
-          </div>
-        </div>
+        <a href="/admin/home">
+          <Lock className="h-8 w-8 hover:cursor-pointer" />
+        </a>
       </div>
     </div>
   );
