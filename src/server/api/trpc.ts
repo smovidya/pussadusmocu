720168107d5b848aeda6b154d00bf6912896a9ef
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-06-11 17:17:24 +0700

    feat: etst

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -70,6 +70,7 @@ export const createCallerFactory = t.createCallerFactory;
  * This is how you create new routers and sub-routers in your tRPC API.
  *
  * @see https://trpc.io/docs/router
+ * 
  */
 export const createTRPCRouter = t.router;
 
