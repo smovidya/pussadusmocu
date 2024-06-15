7baf7ec38efec3d2dbaab7f58706559d0ccf1206
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-06-16 05:39:54 +0700

    feat: add chula sso

--- src/app/users/home/page.tsx
+++ src/app/users/home/page.tsx
@@ -5,39 +5,54 @@ import { ProjectBlog } from "~/app/_components/ProjectsBlog";
 import { RegisterParcel } from "~/app/_components/register-project";
 import { api } from "~/trpc/server";
 import { STUDENT_ID } from "~/utils/constant";
+import { getCookie } from 'cookies-next';
+
 
 const Profile = async () => {
-  const cookieStore = cookies();
-  const student_id = cookieStore.get("student_id");
+  const student_id = getCookie('student_id', { cookies });
   console.log("COOKIE", student_id);
-  const projects: Project[] = await api.project.getProjectByStudent({
-    student_id: STUDENT_ID,
-  });
-  const student = await api.auth.getUser({
-    student_id: student_id?.value ?? "",
-  });
 
-  return (
-    <div className="flex h-full w-full flex-col gap-2">
-      <NavbarUser />
-      <div className="flex flex-row justify-center gap-6">
-        <div className="grid grid-cols-5 gap-2">
-          {projects.map((project) => (
-            <a
-              key={project.project_id}
-              href={`/users/shipping/${project.project_id}`}
-            >
-              <ProjectBlog project={project} />
-            </a>
-          ))}
-        </div>
-        <div className="item-end flex w-24 flex-col gap-1 font-noto-sans">
-          <RegisterParcel />
-          <p>{student_id?.value}</p>
+  if (!student_id) {
+    console.error("Student ID cookie is missing");
+    return <div>Error: Student ID cookie is missing</div>;
+  }
+
+  try {
+    const projects: Project[] = await api.project.getProjectByStudent({
+      student_id: STUDENT_ID,
+    });
+    console.log("PROJECTS", projects);
+
+    const student = await api.auth.getUser({
+      student_id: student_id
+    });
+    console.log("STUDENT", student);
+
+    return (
+      <div className="flex h-full w-full flex-col gap-2">
+        <NavbarUser />
+        <div className="flex flex-row justify-center gap-6">
+          <div className="grid grid-cols-5 gap-2">
+            {projects.map((project) => (
+              <a
+                key={project.project_id}
+                href={`/users/shipping/${project.project_id}`}
+              >
+                <ProjectBlog project={project} />
+              </a>
+            ))}
+          </div>
+          <div className="item-end flex w-24 flex-col gap-1 font-noto-sans">
+            <RegisterParcel />
+            <p>{student?.name}</p>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } catch (error) {
+    console.error("Error fetching data", error);
+    return <div>Error fetching data</div>;
+  }
 };
 
 export default Profile;
