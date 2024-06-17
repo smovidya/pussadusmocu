c77b5d220ff5de202e46a02712700d684bd5a859
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-06-18 01:52:38 +0700

    feat: add middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,13 +3,13 @@ import { type NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const cookie = request.cookies.has("student_id");
-  if (!cookie) {
-    return NextResponse.rewrite("/login");
+  if (cookie || process.env.NODE_ENV === "development") {
+    return NextResponse.rewrite(request.url);
   }
 
-  return NextResponse.rewrite(request.url);
+  return NextResponse.rewrite("https://pussadusmocu.vercel.app/login");
 }
 
 export const config = {
-  matcher: "/users/:path*",
+  matcher: ["/users/:path*", "/admin/:path*"],
 };
