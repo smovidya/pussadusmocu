2eadbb8a49c1bd558605c826451c8f4f0f0006b6
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-06-18 00:52:42 +0700

    feat: add middle ware

--- middleware.ts
+++ middleware.ts
@@ -3,8 +3,7 @@ import { type NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const cookie = request.cookies.get("student_id");
-  if (!cookie?.value) {
-    console.log("PASS");
+  if (!cookie) {
     return NextResponse.redirect("/login");
   }
 
