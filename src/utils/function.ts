41255a41754ff49cd55beb2b7b90c225e7426965
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-06-20 11:39:24 +0700

    chore: add aes-ctr encrypt

--- src/utils/function.ts
+++ src/utils/function.ts
@@ -1,11 +1,10 @@
 import crypto from "crypto";
 import { STUDENT_ID } from "./constant";
 
-// Function to encrypt text using ChaCha20
 export const encrypt = (text: string, key: string) => {
-  const iv = crypto.randomBytes(12); // ChaCha20 typically uses a 12-byte nonce
+  const iv = crypto.randomBytes(16); // AES-CTR typically uses a 16-byte IV
   const cipher = crypto.createCipheriv(
-    "chacha20-poly1305",
+    "aes-256-ctr",
     Buffer.from(key, "hex"),
     iv,
   );
@@ -14,7 +13,6 @@ export const encrypt = (text: string, key: string) => {
   return `${iv.toString("hex")}:${encrypted}`;
 };
 
-// Function to decrypt text using ChaCha20
 export const decrypt = (encrypted: string, key: string) => {
   if (encrypted === "") {
     return process.env.NODE_ENV === "development" ? STUDENT_ID : "default";
@@ -22,7 +20,7 @@ export const decrypt = (encrypted: string, key: string) => {
   const [ivHex, encryptedText] = encrypted.split(":");
   const iv = Buffer.from(ivHex ?? "", "hex");
   const decipher = crypto.createDecipheriv(
-    "chacha20-poly1305",
+    "aes-256-ctr",
     Buffer.from(key, "hex"),
     iv,
   );
