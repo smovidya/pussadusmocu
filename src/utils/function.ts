416e63ff20d54db2ba8072360560079a691c5662
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-06-20 13:57:14 +0700

    feat: chacha20

--- src/utils/function.ts
+++ src/utils/function.ts
@@ -2,12 +2,11 @@ import crypto from "crypto";
 import { STUDENT_ID } from "./constant";
 
 export const encrypt = (text: string, key: string) => {
-  const iv = crypto.randomBytes(16); // AES-CTR typically uses a 16-byte IV
-  const cipher = crypto.createCipheriv(
-    "aes-256-ocb",
-    Buffer.from(key, "hex"),
-    iv,
-  );
+  const iv = crypto.randomBytes(12); // AES-CTR typically uses a 16-byte IV
+
+  const cipher = crypto.createCipheriv("chacha20-poly1305", key, iv, {
+    authTagLength: 16,
+  });
   let encrypted = cipher.update(text, "utf8", "hex");
   encrypted += cipher.final("hex");
   return `${iv.toString("hex")}:${encrypted}`;
@@ -19,11 +18,7 @@ export const decrypt = (encrypted: string, key: string) => {
   }
   const [ivHex, encryptedText] = encrypted.split(":");
   const iv = Buffer.from(ivHex ?? "", "hex");
-  const decipher = crypto.createDecipheriv(
-    "aes-256-ocb",
-    Buffer.from(key, "hex"),
-    iv,
-  );
+  const decipher = crypto.createDecipheriv("chacha20-poly1305", key, iv);
   let decrypted = decipher.update(encryptedText ?? "", "hex", "utf8");
   decrypted += decipher.final("utf8");
   return decrypted;
