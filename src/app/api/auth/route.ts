30dd8a9da010f4d5a45a2385ff6381f80b880ff0
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-06-20 10:52:00 +0700

    chore: add ctr

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,7 +1,6 @@
 import { cookies } from "next/headers";
 import { type NextRequest, NextResponse } from "next/server";
 import { encrypt } from "~/utils/function";
-import crypto from "crypto";
 import { DeeAppId, DeeAppSecret, encryptionKey } from "~/utils/constant";
 
 type UserData = {
