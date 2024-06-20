504dde9091205d809979532219f35a87c38fa675
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-06-20 14:03:46 +0700

    feat: chacha20

--- src/utils/function.ts
+++ src/utils/function.ts
@@ -1,12 +1,23 @@
 import crypto from "crypto";
 import { STUDENT_ID } from "./constant";
 
+function generate32ByteKey(key: string): Buffer {
+  const hash = crypto.createHash("sha256");
+  hash.update(key);
+  return hash.digest();
+}
+
 export const encrypt = (text: string, key: string) => {
   const iv = crypto.randomBytes(12); // AES-CTR typically uses a 16-byte IV
 
-  const cipher = crypto.createCipheriv("chacha20-poly1305", key, iv, {
-    authTagLength: 16,
-  });
+  const cipher = crypto.createCipheriv(
+    "chacha20-poly1305",
+    generate32ByteKey(key),
+    iv,
+    {
+      authTagLength: 16,
+    },
+  );
   let encrypted = cipher.update(text, "utf8", "hex");
   encrypted += cipher.final("hex");
   return `${iv.toString("hex")}:${encrypted}`;
@@ -18,7 +29,11 @@ export const decrypt = (encrypted: string, key: string) => {
   }
   const [ivHex, encryptedText] = encrypted.split(":");
   const iv = Buffer.from(ivHex ?? "", "hex");
-  const decipher = crypto.createDecipheriv("chacha20-poly1305", key, iv);
+  const decipher = crypto.createDecipheriv(
+    "chacha20-poly1305",
+    generate32ByteKey(key),
+    iv,
+  );
   let decrypted = decipher.update(encryptedText ?? "", "hex", "utf8");
   decrypted += decipher.final("utf8");
   return decrypted;
