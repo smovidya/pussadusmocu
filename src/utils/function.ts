c1de3fd5386a4a4a291fcbc4beaa3e9a03865ea7
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-06-20 14:24:43 +0700

    feat: gcm

--- src/utils/function.ts
+++ src/utils/function.ts
@@ -1,39 +1,32 @@
 import crypto from "crypto";
 import { STUDENT_ID } from "./constant";
 
-function generate32ByteKey(key: string): Buffer {
-  const hash = crypto.createHash("sha256");
-  hash.update(key);
-  return hash.digest();
-}
+const algorithm = "aes-256-gcm";
 
 export const encrypt = (text: string, key: string) => {
-  const iv = crypto.randomBytes(12); // AES-CTR typically uses a 16-byte IV
-
+  const iv = crypto.randomUUID(); // Use a secure random IV
   const cipher = crypto.createCipheriv(
-    "chacha20-poly1305",
-    generate32ByteKey(key),
-    iv,
-    {
-      authTagLength: 16,
-    },
+    algorithm,
+    Buffer.from(key, "hex"),
+    Buffer.from(iv, "hex"),
   );
   let encrypted = cipher.update(text, "utf8", "hex");
   encrypted += cipher.final("hex");
-  return `${iv.toString("hex")}:${encrypted}`;
+  const tag = cipher.getAuthTag().toString("hex"); // Get the authentication tag
+  return `${iv}:${tag}:${encrypted}`; // Include the IV, authentication tag, and encrypted text
 };
 
 export const decrypt = (encrypted: string, key: string) => {
   if (encrypted === "") {
     return process.env.NODE_ENV === "development" ? STUDENT_ID : "default";
   }
-  const [ivHex, encryptedText] = encrypted.split(":");
-  const iv = Buffer.from(ivHex ?? "", "hex");
+  const [iv, tag, encryptedText] = encrypted.split(":");
   const decipher = crypto.createDecipheriv(
-    "chacha20-poly1305",
-    generate32ByteKey(key),
-    iv,
+    algorithm,
+    Buffer.from(key, "hex"),
+    Buffer.from(iv ?? "", "hex"),
   );
+  decipher.setAuthTag(Buffer.from(tag ?? "", "hex")); // Set the authentication tag
   let decrypted = decipher.update(encryptedText ?? "", "hex", "utf8");
   decrypted += decipher.final("utf8");
   return decrypted;
