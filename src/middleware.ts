69f3c91ec938ae87c3976ff2db47ad826d990f9b
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-06-25 01:53:06 +0700

    feat: update min and max in input

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,16 @@
 import { NextResponse } from "next/server";
 import { type NextRequest } from "next/server";
+import { decrypt } from "./utils/function";
+import { encryptionKey } from "./utils/constant";
 
 export function middleware(request: NextRequest) {
-  const cookie = request.cookies.has("student_id");
-  if (cookie || process.env.NODE_ENV === "development") {
+  const cookie = request.cookies.get("student_id");
+  const decrypt_cookie = decrypt(cookie?.value ?? "", encryptionKey);
+
+  if (
+    (decrypt_cookie.length === 10 && decrypt_cookie.endsWith("23")) ||
+    process.env.NODE_ENV === "development"
+  ) {
     return NextResponse.rewrite(request.url);
   }
 
