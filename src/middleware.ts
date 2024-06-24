dbb23a65a4ce063570dce74830a6497449aeaad7
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-06-25 02:14:37 +0700

    fix: middleware cant decrypr

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,16 +6,13 @@ import { encryptionKey } from "./utils/constant";
 export function middleware(request: NextRequest) {
   const cookie = request.cookies.get("student_id");
   const decrypt_cookie = decrypt(cookie?.value ?? "", encryptionKey);
+  console.log(decrypt_cookie);
 
-  // if (
-  //   (decrypt_cookie.length === 10 && decrypt_cookie.endsWith("23")) ||
-  //   process.env.NODE_ENV === "development"
-  // ) {
-  //   return NextResponse.rewrite(request.url);
-  // }
+  if (cookie?.value ?? process.env.NODE_ENV === "development") {
+    return NextResponse.rewrite(request.url);
+  }
 
-  // return NextResponse.rewrite("https://pussadusmocu.vercel.app/login");
-  return NextResponse.json(decrypt_cookie);
+  return NextResponse.rewrite("https://pussadusmocu.vercel.app/login");
 }
 
 export const config = {
