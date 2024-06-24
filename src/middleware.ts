1d886df71c88153a7040019aff78b8f2551f0b2b
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-06-25 02:20:30 +0700

    fix: middleware cant decrypr

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,8 @@
 import { NextResponse } from "next/server";
 import { type NextRequest } from "next/server";
-import { decrypt } from "./utils/function";
-import { encryptionKey } from "./utils/constant";
 
 export function middleware(request: NextRequest) {
   const cookie = request.cookies.get("student_id");
-  const decrypt_cookie = decrypt(cookie?.value ?? "", encryptionKey);
-  console.log(decrypt_cookie);
-
   if (cookie?.value ?? process.env.NODE_ENV === "development") {
     return NextResponse.rewrite(request.url);
   }
