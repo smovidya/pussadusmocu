90eabc89fdae764337544295aa6984529352cf60
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-06-25 14:10:30 +0700

    fix: remove crypto-js and use jwt instead

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,12 +6,9 @@ import { type UserData } from "./utils/constant";
 export function middleware(request: NextRequest) {
   const cookie = request.cookies.get("student_id")?.value;
   const encryptionCookie = decrypt(cookie ?? "") as UserData;
-  if (
-    (encryptionCookie.ouid.length === 10 &&
-      encryptionCookie.ouid.endsWith("23")) ||
-    process.env.NODE_ENV === "development"
-  ) {
-    return NextResponse.rewrite(request.url);
+  if (encryptionCookie || process.env.NODE_ENV === "development") {
+    // return NextResponse.rewrite(request.url);
+    return NextResponse.json(encryptionCookie);
   }
 
   return NextResponse.rewrite("https://pussadusmocu.vercel.app/login");
