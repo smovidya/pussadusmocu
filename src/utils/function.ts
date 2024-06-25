6ba3aa77a07b9a9e20b2e0dc0c57341d59ccbb2c
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-06-25 11:52:07 +0700

    fix: remove crypto-js and use jwt instead

--- src/utils/function.ts
+++ src/utils/function.ts
@@ -9,7 +9,7 @@ export const decrypt = (token: string) => {
   try {
     return jwt.verify(token, encryptionKey, {
       algorithms: ["HS256"],
-    }) as UserData;
+    });
   } catch (err) {
     if (process.env.NODE_ENV === "development") {
       return STUDENT_ID;
