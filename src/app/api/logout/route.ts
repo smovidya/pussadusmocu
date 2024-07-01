1cf63510da95c0004dc0fff15780550b961bf007
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-07-01 12:40:48 +0700

    feat: logout system

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -7,6 +7,6 @@ export async function GET(_req: NextRequest) {
   cookieStore.delete("student_id");
 
   return NextResponse.redirect(
-    "https://account.it.chula.ac.th/html/logout?serviceName=PUSSADU-SUCU&service=https://pussadusmocu.vercel.app/login",
+    "https://account.it.chula.ac.th/logout?serviceName=PUSSADU-SUCU&service=https://pussadusmocu.vercel.app/login",
   );
 }
