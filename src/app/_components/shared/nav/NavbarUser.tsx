78756dd4a2bd2852705598bd4ce497e0994ea8ce
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-08-14 00:21:35 +0700

    feat: add key feature and use bun instead of package.lock

--- src/app/_components/shared/nav/NavbarUser.tsx
+++ src/app/_components/shared/nav/NavbarUser.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Home, Lock, LogOut, ShoppingCart, Menu} from "lucide-react";
+import { Home, Lock, LogOut, ShoppingCart, Menu } from "lucide-react";
 import Image from "next/image";
 import { Input } from "~/components/ui/input";
 import { selectedOption } from "~/stores/slices/search";
@@ -20,18 +20,17 @@ export const NavbarUser = () => {
   };
   return (
     <div className="inline-flex h-24 w-full items-center justify-start gap-[38px] bg-yellow01 px-5 py-1.5">
-
       <div className="flex items-center">
         <button
-            className="lg:hidden"
-            onClick={() => setMenuOpen(!menuOpen)}
-            aria-label="Toggle Menu"
-          >
-            <Menu className="h-8 w-8" />
+          className="lg:hidden"
+          onClick={() => setMenuOpen(!menuOpen)}
+          aria-label="Toggle Menu"
+        >
+          <Menu className="h-8 w-8" />
         </button>
         <a
           href="/users/home"
-          className="items-center p-4 justify-center hover:cursor-pointer"
+          className="items-center justify-center p-4 hover:cursor-pointer"
         >
           <Image
             src={"/picture/yellowBox.svg"}
@@ -42,7 +41,7 @@ export const NavbarUser = () => {
         </a>
       </div>
 
-      <div className="hidden lg:flex w-full font-noto-sans">
+      <div className="hidden w-full font-noto-sans lg:flex">
         <Input
           className="rounded-full"
           placeholder="ค้นหาพัสดุ"
@@ -50,7 +49,7 @@ export const NavbarUser = () => {
         />
       </div>
 
-      <div className="hidden lg:flex items-center justify-start gap-4">
+      <div className="hidden items-center justify-start gap-4 lg:flex">
         <a href="/users/home" title="หน้าหลัก">
           <Home className="h-8 w-8 hover:cursor-pointer" />
         </a>
@@ -66,33 +65,48 @@ export const NavbarUser = () => {
       </div>
 
       {menuOpen && (
-        <div className="lg:hidden absolute top-24 left-0 w-full bg-yellow01 p-5 z-50">
+        <div className="absolute left-0 top-24 z-50 w-full bg-yellow01 p-5 lg:hidden">
           <div className="flex flex-col items-center gap-4">
             <Input
-              className="rounded-full w-full"
+              className="w-full rounded-full"
               placeholder="ค้นหาพัสดุ"
               onChange={handleInputChange}
             />
-            <a href="/users/home" title="หน้าหลัก" className="flex items-center gap-2 text-sm">
-              <Home className="h-8 w-15 hover:cursor-pointer" />
+            <a
+              href="/users/home"
+              title="หน้าหลัก"
+              className="flex items-center gap-2 text-sm"
+            >
+              <Home className="w-15 h-8 hover:cursor-pointer" />
               <span>หน้าหลัก</span>
             </a>
-            <a href="/users/cart" title="พัสดุที่จองไป" className="flex items-center gap-2 text-sm">
-              <ShoppingCart className="h-8 w-15 hover:cursor-pointer" />
+            <a
+              href="/users/cart"
+              title="พัสดุที่จองไป"
+              className="flex items-center gap-2 text-sm"
+            >
+              <ShoppingCart className="w-15 h-8 hover:cursor-pointer" />
               <span>พัสดุที่จองไป</span>
             </a>
-            <a href="/admin/home" title="Admin only" className="flex items-center gap-2 text-sm">
-              <Lock className="h-8 w-15 hover:cursor-pointer" />
+            <a
+              href="/admin/home"
+              title="Admin only"
+              className="flex items-center gap-2 text-sm"
+            >
+              <Lock className="w-15 h-8 hover:cursor-pointer" />
               <span>Admin</span>
             </a>
-            <a href="/api/logout" title="ออกจากระบบ" className="flex items-center gap-2 text-sm">
-              <LogOut className="h-8 w-15 hover:cursor-pointer" />
+            <a
+              href="/api/logout"
+              title="ออกจากระบบ"
+              className="flex items-center gap-2 text-sm"
+            >
+              <LogOut className="w-15 h-8 hover:cursor-pointer" />
               <span>ออกจากระบบ</span>
             </a>
           </div>
         </div>
       )}
-
     </div>
   );
 };
