7b1ff9da6e204f2cbdc29e34574fcc7a4e5cd6c5
bunyawatapp37204 <stonksfarm37204@gmail.com>
2024-12-11 06:32:28 +0700

    feat: version

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -11,8 +11,8 @@ const LoginPage = () => {
   };
 
   // Version and deployment date
-  const version = "1.3.2";
-  const deploymentDate = "Sep 7, 2024";
+  const version = "1.4.0";
+  const deploymentDate = "Dec 11, 2024";
 
   return (
     <div className="grid h-screen w-screen md:grid-cols-5">
