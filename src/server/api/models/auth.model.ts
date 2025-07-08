6c5fec7e8d35238bd8e701581cd5039973254380
Patsagorn <patsagorn_y@outlook.com>
2025-07-08 12:33:27 +0700

    feat: implement addUser mutation with validation and error handling in auth router

--- src/server/api/models/auth.model.ts
+++ src/server/api/models/auth.model.ts
@@ -5,5 +5,35 @@ export const StudentDtoSchema = z.object({
   student_id: z.string(),
 });
 
+export const StudentDepartmentEnum = z.enum([
+  "SMO",
+  "MATHCOM",
+  "MARINE",
+  "CHEM",
+  "CHEMTECH",
+  "BIO",
+  "BIOCHEM",
+  "BSAC",
+  "BBTECH",
+  "FOODTECH",
+  "MATSCI",
+  "PHYSICS",
+  "BOTGEN",
+  "MICROBIO",
+  "PHOTO",
+  "GEO",
+  "ENVI",
+  "NISIT_OFFICER",
+]);
+
+export const StudentSchema = z.object({
+  student_id: z.string(),
+  name: z.string().optional(),
+  email: z.string().email().optional(),
+  department: StudentDepartmentEnum,
+  isAdmin: z.boolean().optional(),
+  line_id: z.string().optional(),
+});
+
 // Infer the TypeScript type from the schema
 export type StudentDto = z.infer<typeof StudentDtoSchema>;
--- src/server/api/routers/auth.router.ts
+++ src/server/api/routers/auth.router.ts
@@ -1,6 +1,13 @@
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {
+  adminOnlyProcedure,
+  createTRPCRouter,
+  publicProcedure,
+} from "~/server/api/trpc";
 import { StudentDtoSchema } from "../models/auth.model";
 import { getUser } from "../services/auth.service";
+import { z } from "zod";
+import { TRPCError } from "@trpc/server";
+import { Department } from "@prisma/client";
 
 /**
  * TRPC Router for handling authentication-related operations.
@@ -17,5 +24,43 @@ export const authRouter = createTRPCRouter({
     .input(StudentDtoSchema)
     .query(async ({ ctx, input }) => {
       return getUser(ctx, input);
+    }),
+
+  addUser: adminOnlyProcedure
+    .input(
+      z.object({
+        student_id: z.string(),
+        name: z.string().optional(),
+        email: z.string().email().optional(),
+        department: z
+          .enum(Object.values(Department) as [string, ...string[]])
+          .optional(),
+        isAdmin: z.boolean().optional(),
+        line_id: z.string().optional(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { student_id, name, department, email, isAdmin, line_id } = input;
+
+      const existingUser = await ctx.db.student.findUnique({
+        where: { student_id },
+      });
+      if (existingUser) {
+        throw new TRPCError({
+          code: "CONFLICT",
+          message: `User with student_id ${student_id} already exists.`,
+        });
+      }
+      const newUser = await ctx.db.student.create({
+        data: {
+          student_id,
+          name,
+          email: email ?? "",
+          department: (department ?? "SMO") as Department,
+          isAdmin: isAdmin ?? false,
+          line_id: line_id ?? "",
+        },
+      });
+      return newUser;
     }),
 });
